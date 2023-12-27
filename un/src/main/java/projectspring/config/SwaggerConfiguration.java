ecf9b7da0e7957faf013f761404730b9874baab4
lucatdat <luc.duperron@laposte.net>
2023-12-27 20:03:12 +0100

    UPT: Maj code avec ajout annotations Swagger

--- un/src/main/java/projectspring/config/SwaggerConfiguration.java
+++ un/src/main/java/projectspring/config/SwaggerConfiguration.java
@@ -1,5 +1,10 @@
 package projectspring.config;
 
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
 public class SwaggerConfiguration {
     
 }
